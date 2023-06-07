5795999743843c255dda770e86f26cffccb80af2
Looog6 <120652500+Looog6@users.noreply.github.com>
2023-06-07 20:06:12 +0400

    Update exchange.py

--- exchange.py
+++ exchange.py
@@ -10,25 +10,24 @@ class Exchange:
     @staticmethod
     def get_price(base: str, quote: str, amount: str):
         if quote == base:
-            raise ExchangeException(
-                f'Нельзя конвертировать одинаковые валюты {base}.')
+            raise ExchangeException(f'Нельзя конвертировать одинаковые валюты {base}.')
 
         try:
-            quote_ticker = keys[quote]
+            quote_ticker = keys[quote.lower()]
         except KeyError:
-            raise ExchangeException(f'Не смог обработать валюту {quote}')
+            raise ExchangeException(f'Не смог обработать валюту {quote.lower()}')
 
         try:
-            base_ticker = keys[base]
+            base_ticker = keys[base.lower()]
         except KeyError:
-            raise ExchangeException(f'Не смог обработать валюту {base}')
+            raise ExchangeException(f'Не смог обработать валюту {base.lower()}')
 
         try:
-            amount = int(amount)
+            amount = float(amount)
         except ValueError:
             raise ExchangeException(f'Не смог обработать количество {amount}')
 
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
-        total_base = float(json.loads(r.content)[keys[quote]])
+        total_base = float(json.loads(r.content)[keys[quote.lower()]])
         return round(total_base*amount, 6) #Округление после запятой
     
